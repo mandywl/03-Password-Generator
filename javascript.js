995b6b664228ffc49c7c7a012d60e28e8627372b
AU\mandy.wells-lakeland <mandy.wellslakeland@akqa.com>
2020-02-20 14:16:00 +1100

    Added validation on forms

--- javascript.js
+++ javascript.js
@@ -3,22 +3,63 @@ function clearFields() {
   document.getElementById("text").innerHTML = "";
 }
 
-function makeid(length) {
+function makeid(characters) {
   var result = "";
-  var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
+  var randomLength = Math.floor(Math.random() * 128);
+  console.log(randomLength);
+  for (var i = 0; i < randomLength; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
 
 function myFunction() {
-  var checkBox = document.getElementById("moreThanEightCharacters");
-  var passwordResult = makeid(8);
-  if (checkBox.checked == true) {
-    console.log("hello");
-    $("#text").append(passwordResult);
+  var characters;
+  var eightCharacters = document.getElementById("moreThanEightCharacters");
+  var lowercase = document.getElementById("lowercase");
+  var uppercase = document.getElementById("uppercase");
+  var numeric = document.getElementById("numeric");
+  var special = document.getElementById("specialCharacters");
+
+  if (
+    lowercase.checked &&
+    uppercase.checked &&
+    numeric.checked &&
+    special.checked
+  ) {
+    characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()~_+";
+  } else if (lowercase.checked && uppercase.checked && numeric.checked) {
+    characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  } else if (lowercase.checked && special.checked && numeric.checked) {
+    characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()~_+";
+  } else if (special.checked && uppercase.checked && numeric.checked) {
+    characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()~_+";
+  } else if (lowercase.checked && uppercase.checked) {
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  } else if (lowercase.checked && numeric.checked) {
+    characters = "abcdefghijklmnopqrstuvwxyz1234567890";
+  } else if (lowercase.checked && special.checked) {
+    characters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()~_+";
+  } else if (uppercase.checked && numeric.checked) {
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  } else if (uppercase.checked && special.checked) {
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()~_+";
+  } else if (numeric.checked && special.checked) {
+    characters = "1234567890!@#$%^&*()~_+";
+  } else if (uppercase.checked == true) {
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if (numeric.checked == true) {
+    characters = "1234567890";
+  } else if (special.checked == true) {
+    characters = "!@#$%^&*()~_+";
+  } else if (lowercase.checked == true) {
+    characters = "abcdefghijklmnopqrstuvwxyz";
+  } else {
+    alert("You must select one option from Character Types");
   }
+  var passwordResult = makeid(characters);
+  $("#text").append(passwordResult);
 }
