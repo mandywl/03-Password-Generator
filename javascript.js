7b04b858071d924d765491ef14e0fa4dd9d3c5c2
AU\mandy.wells-lakeland <mandy.wellslakeland@akqa.com>
2020-02-21 17:13:08 +1100

    Removed the commented code

--- javascript.js
+++ javascript.js
@@ -15,54 +15,6 @@ function makeid(characters) {
 }
 
 function myFunction() {
-  // var characters;
-  // var eightCharacters = document.getElementById("moreThanEightCharacters");
-  // var lowercase = document.getElementById("lowercase");
-  // var uppercase = document.getElementById("uppercase");
-  // var numeric = document.getElementById("numeric");
-  // var special = document.getElementById("specialCharacters");
-
-  // if (
-  //   lowercase.checked &&
-  //   uppercase.checked &&
-  //   numeric.checked &&
-  //   special.checked
-  // ) {
-  //   characters =
-  //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()~_+";
-  // } else if (lowercase.checked && uppercase.checked && numeric.checked) {
-  //   characters =
-  //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-  // } else if (lowercase.checked && special.checked && numeric.checked) {
-  //   characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()~_+";
-  // } else if (special.checked && uppercase.checked && numeric.checked) {
-  //   characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()~_+";
-  // } else if (lowercase.checked && uppercase.checked) {
-  //   characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  // } else if (lowercase.checked && numeric.checked) {
-  //   characters = "abcdefghijklmnopqrstuvwxyz1234567890";
-  // } else if (lowercase.checked && special.checked) {
-  //   characters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()~_+";
-  // } else if (uppercase.checked && numeric.checked) {
-  //   characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-  // } else if (uppercase.checked && special.checked) {
-  //   characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()~_+";
-  // } else if (numeric.checked && special.checked) {
-  //   characters = "1234567890!@#$%^&*()~_+";
-  // } else if (uppercase.checked == true) {
-  //   characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  // } else if (numeric.checked == true) {
-  //   characters = "1234567890";
-  // } else if (special.checked == true) {
-  //   characters = "!@#$%^&*()~_+";
-  // } else if (lowercase.checked == true) {
-  //   characters = "abcdefghijklmnopqrstuvwxyz";
-  // } else {
-  //   alert("You must select one option from Character Types");
-  // }
-  // var passwordResult = makeid(characters);
-  // $("#text").append(passwordResult);
-
   var lowercaseEl = document.getElementById("lowercase");
   var uppercaseEl = document.getElementById("uppercase");
   var numericEl = document.getElementById("numeric");
